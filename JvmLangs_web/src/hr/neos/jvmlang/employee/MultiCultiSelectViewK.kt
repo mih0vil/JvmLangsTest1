81f7950e78ce68200736adee1dc426ff58ffcc80
Mihovil Frater <mihovil.frater@neos.hr>
2016-12-15 17:24:57 +0100

    web gradle

--- JvmLangs_web/src/hr/neos/jvmlang/employee/MultiCultiSelectViewK.kt
+++ JvmLangs_web/src/hr/neos/jvmlang/employee/MultiCultiSelectViewK.kt
@@ -1,14 +1,22 @@
 package hr.neos.jvmlang.employee
 
 import javax.inject.Named
+
 import org.omnifaces.cdi.ViewScoped
 import java.io.Serializable
+import hr.neos.jvmlang.kotlin.EmployeeServiceK
+import javax.ejb.EJB
+import hr.neos.jvmlang.kotlin.EmployeeServiceKLocal
+
 
 @Named
 @ViewScoped
 open class MultiCultiSelectViewK : Serializable {
 
-	val test = "Testing Kotlin"
-		
+	@EJB
+	private var employeeService : EmployeeServiceKLocal? = null  
+	
+	open val test = "Testing Kotlin"
+	open val test2 = employeeService?.getAllHavingDepartments()
 	
 }
